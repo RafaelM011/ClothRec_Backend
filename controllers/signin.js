17f4e7ba2f7fe4695e10b6c76fdd615ddcc8fcbe
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-23 22:54:56 -0400

    <FEAT> signin feature added

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,14 @@
 export const signIn = (db, bcrypt) => (req, res) => {
-
+  const { username, password } = req.body
+  db('login')
+    .select('*')
+    .where({ username })
+    .then(data => {
+      const { hash } = data[0]
+      const isValid = bcrypt.compareSync(password, hash)
+      if (isValid) {
+        res.status(200).json(isValid)
+      }
+    })
+    .catch(() => res.status(400).json('Username or password does not exists'))
 }
